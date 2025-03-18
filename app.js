97f61dd31eae946deeaba1ad9dde5c985252a1c9
Nima <nimixfaghih@gmail.com>
2025-03-18 16:48:55 +0330

    added swagger

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const express= require('express');
 const app=express();
 const routes=require('./routes/userRoutes');
 const dbRoutes=require('./routes/dbRoutes');
+const YAML=require('yamljs');
+const swaggerUi=require('swagger-ui-express');
+const path=require('path');
+const swaggerDocument=YAML.load(path.join(__dirname,'swagger.yaml'));
 app.use(express.urlencoded({ extended: true })); 
 
 app.use(express.json());
+
+app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerDocument));
 
 app.use("/api/users", routes);
 app.use("/api/users/db", dbRoutes);
